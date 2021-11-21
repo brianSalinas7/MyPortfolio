6d1cd4b27dd0e5233eb1c1eb75063b70125ce9a2
Brian Paul Salinas <braiiann.1998@gmail.com>
2021-11-21 03:58:50 -0300

    cards end

---
+++ js/cards.js
@@ -0,0 +1,10 @@
+const info = document.querySelector('.info');
+const contentCard = document.querySelector('#content-bottom')
+const card = document.querySelector('.card')
+
+
+info.addEventListener('mouseover', ()=>{
+    info.style.color= red
+})
+
+
